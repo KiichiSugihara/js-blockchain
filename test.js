f78a50be9d1076177f0e340d434f71ec0e80f6ce
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-07-28 23:37:19 +0900

    add three block

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain()
 
-bitcoin.createNewBlock(
-  7653,
-  '00KNWRUBWEJWENFOJNWO',
-  '07HDFSKBWESUFBWEIBWIEUFBNW'
-)
+bitcoin.createNewBlock()
+
+bitcoin.createNewBlock(7653, '00KNWRUBWEJWENFOJNWO', '00NDGHEWNEJWHBRNMNWO')
+bitcoin.createNewBlock(8971, '00HDNFHEWEDGRBCHRNKG', '00HDYENRHFBKDURNFHNE')
+bitcoin.createNewBlock(9761, '00JOIRNNOIHWEOUBNEWO', '00NJKRUOQWNOIWHRNOWQ')
 
 console.log(bitcoin)
