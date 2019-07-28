068bc9a713e49f43612eb573781b8c56274a6389
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-07-28 23:42:02 +0900

    connect chain

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain()
-
 bitcoin.createNewBlock(80103, '00KNWRUBWEJWENFOJNWO', '00NDGHEWNEJWHBRNMNWO')
 
 bitcoin.createNewTransaction(1, 'ALICEJSJSNWNN', 'BOBDKENINOMDO')
+
+bitcoin.createNewBlock(99282, '00HDNFHEWEDGRBCHRNKG', '00HDYENRHFBKDURNFHNE')
 
 console.log(bitcoin)
